7010121b5ea1813adcf744318ccb042fd3ce7f51
Sasha Savenko <sashasavenko3@gmail.com>
2025-09-11 22:45:11 +0300

    carcard

--- src/components/carCard/CarCard.tsx
+++ src/components/carCard/CarCard.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import type { carCardProps } from '../../utils/types'
 import css from './CarCard.module.css'
+import { Link } from 'react-router-dom'
 
-const CarCard: React.FC<carCardProps> = ({car: {img, type, year, price}}) => {
+const CarCard: React.FC<carCardProps> = ({car: {img, type, year, model, brand, rentalPrice, id, rentalCompany, mileage}}) => {
   return (
     <div>
-      <img src={img} alt={type} />
+      <div className={css.imgWrap}>
+      <img src={img} alt={type} /></div>
+      <p>{brand} <span>{model}</span> <span>{year}</span> </p>
+<p>{`$${rentalPrice}`}</p>
+<p>{rentalCompany}</p>
+<p>{type}</p>
+<p>{mileage}</p>
+      <Link to={`/catalog/:${id}`}>Learn more</Link>
     </div>
   )
 }
--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom'
 
 const MainPage = () => {
   return (
-    <div className={css.mainWrap}>
-      <div>
+    <div className={`${css.mainWrap} container`}>
+      <div className={css.textWrap}>
       <h1>Find your perfect rental car</h1>
       <p>Reliable and budget-friendly rentals for any journey</p>
     <NavLink to={"/catalog"}>View Catalog</NavLink>
