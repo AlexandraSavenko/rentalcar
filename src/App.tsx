950cf5741d99171e1a332c3cd2bf4e7206ff6619
Sasha Savenko <sashasavenko3@gmail.com>
2025-09-11 15:10:09 +0300

    routing in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Layout from './components/layout/Layout'
 import MainPage from './pages/mainPage/MainPage'
+import CatalogPage from './pages/catalogPage/CatalogPage'
 
 function App() {
 
   return (<>
+  <Layout>
+    <Routes>
+<Route path="/" element={<MainPage/>} />
+<Route path="/catalog" element={<CatalogPage/>} />
+<Route path="/catalog/:id" element={<MainPage/>} />
+    </Routes>
+  </Layout>
     <h1>
       rentalcars
     </h1>
