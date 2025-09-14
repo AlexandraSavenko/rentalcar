bbc53f146c4f06deb6edb5387381b06529dc924b
Sasha Savenko <sashasavenko3@gmail.com>
2025-09-14 18:48:23 +0300

    infoWrap styles

--- src/pages/carPage/CarPage.tsx
+++ src/pages/carPage/CarPage.tsx
@@ -21,44 +21,59 @@ const CarPage = () => {
     return <p>Sorry, something went wrong</p>;
   }
   return (
-    <>
-    <div className={css.imageWrap}>
-      <img src={car.img} alt={car.description} />
+    <div className={`${css.carWrap} container`}>
+      <div className={css.imageWrap}>
+        <img src={car.img} alt={car.description} />
       </div>
       <div className={css.infoWrap}>
         <h3>
           {car.brand} {car.model}, {car.year}
         </h3>
-        <div>
+        <div className={css.originandage}>
           <p>{whereFrom}</p>
           <p>Mileage: {car.mileage} km</p>
         </div>
-        <p>$ {car.rentalPrice}</p>
-        <p>{car.description}</p>
-        <h3>Rentsl Conditions:</h3>
-        <ul>{RC.length > 0 && RC.map((el, i) => <li key={i}>{el}</li>)}</ul>
-        <h3>Car Specifications:</h3>
-        <ul>
-          <li>
-            <span>Year: {car.year}</span>
-          </li>
-          <li>
-            <span>Type: {car.type}</span>
-          </li>
-          <li>
-            <span>Fuel Consumption: {car.fuelConsumption}</span>
-          </li>
-          <li>
-            <span>Engine Size: {car.engineSize}</span>
-          </li>
-        </ul>
+        <p className={css.price}>$ {car.rentalPrice}</p>
+        <p className={css.description}>{car.description}</p>
+        <div className={css.listsWrap}>
+            <div className={css.list}>
 
-        <h3>Accessories and functionalities:</h3>
-        <ul>
-          {AandF.length > 0 && AandF.map((el, i) => <li key={i}>{el}</li>)}
-        </ul>
+<h3>Rentsl Conditions:</h3>
+          <ul>{RC.length > 0 && RC.map((el, i) => <li key={i}>{el}</li>)}</ul>
+
+            </div>
+          <div className={css.list}>
+
+<h3>Car Specifications:</h3>
+          <ul>
+            <li>
+              <span>Year: {car.year}</span>
+            </li>
+            <li>
+              <span>Type: {car.type}</span>
+            </li>
+            <li>
+              <span>Fuel Consumption: {car.fuelConsumption}</span>
+            </li>
+            <li>
+              <span>Engine Size: {car.engineSize}</span>
+            </li>
+          </ul>
+
+            </div>
+          
+          <div className={css.list}>
+
+<h3>Accessories and functionalities:</h3>
+          <ul>
+            {AandF.length > 0 && AandF.map((el, i) => <li key={i}>{el}</li>)}
+          </ul>
+
+            </div>
+          
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
