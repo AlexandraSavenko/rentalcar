6e67ddd5eb2074c71f13043228640031978ad857
Sasha Savenko <sashasavenko3@gmail.com>
2025-09-20 20:15:38 +0300

    readme added

--- src/components/filterSelect/FilterSelect.tsx
+++ src/components/filterSelect/FilterSelect.tsx
@@ -4,6 +4,9 @@ import css from "./FilterSelect.module.css"
 import type { FilterSelectProps } from '../../utils/types'
 
 const FilterSelect: React.FC<FilterSelectProps> = ({name, options}) => {
+  if(!options){
+    return <p>Sorry, you can't use filters at the moment</p>
+  }
   return (
     <Field as="select" name={name} className={css.filterSelect}>
       <option value="" disabled>
